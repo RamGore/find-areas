2e584950facafe4ab842d798dcfc1d0698514c33
prover11 <130224491+prover11@users.noreply.github.com>
2023-04-09 14:28:13 +0530

    Add files via upload

---
+++ area of circle.c
@@ -0,0 +1,11 @@
+//Find the area of circle.
+#include<stdio.h>
+float pi=3.14;
+int main()
+{
+    int a,r;
+    printf("Enter the number: ");
+    scanf("%d",&a);
+    r=pi*a*a;
+    printf("Area of Circle: %d",r);
+}
